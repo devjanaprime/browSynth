b8b66b07a70086bfe6460483698bdd39d0864e1f
Dev Jana <dev@primeacademy.io>
2019-03-06 13:51:45 -0600

    updated frequencies with names and included addNoteToScale function

---
+++ scripts/scales.js
@@ -0,0 +1,50 @@
+let scales = [
+    {
+        names: [ 'A Major', 'F# Minor' ],
+        notes: [ 'A', 'B', 'C#', 'D', 'E', 'F#', 'G#' ]
+    },
+    {
+        names: [ 'Bb Major', 'G Minor' ],
+        notes: [ 'Bb', 'C', 'D', 'Eb', 'F', 'G', 'A' ]
+    },
+    {
+        names: [ 'B Major', 'G# Minor' ],
+        notes: [ 'B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#' ]
+    },
+    {
+        names: [ 'C Major', 'A Minor' ],
+        notes: [ 'C', 'D', 'E', 'F', 'G', 'A', 'B' ]
+    },
+    {
+        names: [ 'Db Major', 'Bb Minor' ],
+        notes: [ 'Db', 'E', 'F', 'Gb', 'Ab', 'Bb', 'C' ]
+    },
+    {
+        names: [ 'D Major', 'B Minor' ],
+        notes: [ 'D', 'E', 'F#', 'G', 'A', 'B', 'C#' ]
+    },
+    {
+        names: [ 'Eb Major', 'C Minor' ],
+        notes: [ 'Eb', 'F', 'G', 'Ab', 'Bb', 'C', 'D' ]
+    },
+    {
+        names: [ 'E Major', 'C# Minor' ],
+        notes: [ 'E', 'F#', 'G#', 'A', 'B', 'C#', 'D#' ]
+    },
+    {
+        names: [ 'F Major', 'D Minor' ],
+        notes: [ 'F', 'G', 'A', 'Bb', 'C', 'D', 'E' ]
+    },
+    {
+        names: [ 'F# Major', 'D# Minor' ],
+        notes: [ 'F#', 'G#', 'A#', 'B', 'C#', 'D#', 'E#' ]
+    },
+    {
+        names: [ 'G Major', 'E Minor' ],
+        notes: [ 'G', 'A', 'B', 'C', 'D', 'E', 'F#' ]
+    },
+    {
+        names: [ 'Ab Major', 'F Minor' ],
+        notes: [ 'Ab', 'Bb', 'C', 'Db', 'Eb', 'F', 'G' ]
+    },
+]
